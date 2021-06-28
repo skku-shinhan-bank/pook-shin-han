de093b8683c4853698a568825914c5aaaa942a9d
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2021-06-28 17:11:49 +0900

    feat:argv 받게 변경

--- Dockerfile
+++ Dockerfile
@@ -16,4 +16,4 @@ RUN pip3 install -r requirements.txt
 # RUN python setup.py install
 
 # container가 구동되면 실행
-ENTRYPOINT ["python", "run.py"]
+ENTRYPOINT ["python", "run.py", "false", "3000"]
--- run.py
+++ run.py
@@ -1,10 +1,11 @@
 from src.app import app
-import os
+import sys
 
 if __name__=="__main__":
   print('START - POOK SHIN HAN')
-  
-  port = 3000
-  debug = False
-  
-  app.run(debug=debug, port=port)
+
+  if len(sys.argv) == 3:
+    debug = sys.argv[1]
+    port = sys.argv[2]
+    
+    app.run(debug=debug, port=port)
