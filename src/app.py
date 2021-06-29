e3576cffa1cebfc4371b4ece5d53e8473c8b79f0
hanjungwoo <ajtwlswjddnv1102@gmail.com>
2021-06-29 15:34:40 +0900

    feat:issue predictor 추가

--- src/app.py
+++ src/app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
 from src.lib.review_comment_generator import ReviewCommentGenertator
+from src.lib.issue_predictor import IssuePredictor
+
+issuePredictor = IssuePredictor()
 
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
+
 
 @app.route('/', methods=('GET', ))
 def index():
@@ -12,9 +16,9 @@ def index():
 def review():
   review = request.json['review']
 
-  print(review)
+  issueClass = issuePredictor.predict(review)
 
   return jsonify({
     'review': review,
-    'comment': ReviewCommentGenertator.generate(0),
+    'comment': ReviewCommentGenertator.generate(issueClass),
   })
---
+++ src/lib/issue_predictor.py
@@ -0,0 +1,9 @@
+class IssuePredictor:
+  def __init__(self):
+    # loaded_model = BERTClassifier(bertmodel,  dr_rate=0.5).to(device)
+    # loaded_model.load_state_dict(torch.load('/content/drive/MyDrive/산학/model_state.pt'))
+    pass
+  
+  def predict(self, review):
+
+    return 0
