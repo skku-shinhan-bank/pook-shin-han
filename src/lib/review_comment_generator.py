a93d95c4d0fabafe6f40fed4636b81de6bf1fb17
unknown <ajtwlswjddnv1102@gmail.com>
2021-08-13 09:01:13 +0900

    feat:답변 형식 변경

--- src/lib/review_comment_generator.py
+++ src/lib/review_comment_generator.py
@@ -3,18 +3,18 @@ class ReviewCommentGenertator:
 
   @staticmethod
   def generate(issueId):
-    common_message = 'issue id: {}\n\n'.format(issueId)
+    common_message = ''
     if issueId == 0:  # 앱 실행
-        return common_message + '안녕하세요. 고객님. 앱 실행에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
+        return common_message + '[앱 실행 기능] 안녕하세요. 고객님. 앱 실행에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
     elif issueId == 1:  # 로그인
-        return common_message + '안녕하세요. 고객님, 로그인에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
+        return common_message + '[로그인 기능] 안녕하세요. 고객님, 로그인에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
     elif issueId == 2:  # 회원가입
-        return common_message + '안녕하세요. 고객님, 회원가입에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
+        return common_message + '[회원가입 기능] 안녕하세요. 고객님, 회원가입에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
     elif issueId == 3:  # 금융 서비스
-        return common_message + '안녕하세요. 고객님, 금융 서비스에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
+        return common_message + '[금융 서비스 기능] 안녕하세요. 고객님, 금융 서비스에 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
     elif issueId == 4:  # 기타
-        return common_message + '안녕하세요, 기타 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
+        return common_message + '[기타 기능] 안녕하세요, 기타 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
     elif issueId == 5:  # 엡 외부 기능
-        return common_message + '안녕하세요, 앱 외부 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
-    return '안녕하세요. 고객님. 문제를 알 수 없습니다.'
+        return common_message + '[앱 외부 기능] 안녕하세요, 앱 외부 문제가 있으신가요??' + ReviewCommentGenertator.__goodByeMessage
+    return '[기능 없음] 안녕하세요. 고객님. 문제를 알 수 없습니다.'
     
